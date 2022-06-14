560708c75611d44d7f276246f533ccd27eb33d6a
PelleErobreren <pelledahl22@gmail.com>
2022-06-14 14:28:06 +0200

    Update Main.java

--- Simpel Chatbot/src/Main.java
+++ Simpel Chatbot/src/Main.java
@@ -16,33 +16,33 @@ public class Main
 
     static void greet(String assistantName, String birthYear)
     {
-        System.out.println("Hello! My name is " + assistantName + ".");
-        System.out.println("I was created in " + birthYear + ".");
-        System.out.println("Please, remind me your name.");
+        System.out.println("Hej! Mit navn er " + assistantName + ".");
+        System.out.println("Jeg blev skabt i " + birthYear + ".");
+        System.out.println("Mind mig venligst om dit navn.");
     }
 
     static void remindName()
     {
         String name = scanner.nextLine();
-        System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Sikke et godt navn du har, " + name + "!");
     }
 
     static void guessAge()
     {
-        System.out.println("Let me guess your age.");
-        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        System.out.println("Lad mig gætte din alder.");
+        System.out.println("Indtast rester af at dividere din alder med 3, 5 og 7.");
 
         int rem3 = scanner.nextInt();
         int rem5 = scanner.nextInt();
         int rem7 = scanner.nextInt();
 
         int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
-        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println("Din alder er " + age + "; det er et godt tidspunkt at begynde at programmere!");
     }
 
     static void count()
     {
-        System.out.println("Now I will prove to you that I can count to any number you want.");
+        System.out.println("Nu vil jeg bevise for dig, at jeg kan tælle til et hvilket som helst tal, du vil.");
         int num = scanner.nextInt();
         for (int i = 0; i <= num; i++)
         {
@@ -52,25 +52,25 @@ public class Main
 
     static void test()
     {
-        System.out.println("Let's test your programming knowledge.");
-        System.out.println("Why do we use methods?");
+        System.out.println("Lad os teste din programmeringsviden.");
+        System.out.println("Hvorfor bruger vi metoder?");
 
-        System.out.println("1. To repeat a statement multiple times.");
-        System.out.println("2. To decompose a program into several small subroutines.");
-        System.out.println("3. To determine the execution time of a program.");
-        System.out.println("4. To interrupt the execution of a program.");
+        System.out.println("1. For at gentage en sætning flere gange.");
+        System.out.println("2. For at dekomponere et program i flere små underrutiner.");
+        System.out.println("3. For at bestemme udførelsestiden for et program.");
+        System.out.println("4. For at afbryde udførelsen af et program.");
 
         int input = scanner.nextInt();
 
         while (input!=2)
         {
-            System.out.println("Please, try again.");
+            System.out.println("Prøv igen.");
             input = scanner.nextInt();
         }
     }
 
     static void end()
     {
-        System.out.println("Congratulations, have a nice day!");
+        System.out.println("Tillykke, hav en god dag!");
     }
 }
